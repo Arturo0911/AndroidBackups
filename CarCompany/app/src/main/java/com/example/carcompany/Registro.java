d08571cbe1b098b536d70ac1cc460fccb0a17fff
Arturo Negreiros <anegreiross@outlook.com>
2021-02-14 00:00:02 -0500

    ok

--- CarCompany/app/src/main/java/com/example/carcompany/Registro.java
+++ CarCompany/app/src/main/java/com/example/carcompany/Registro.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -16,7 +19,9 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.carcompany.process.Credential;
 import com.example.carcompany.process.ListControl;
@@ -24,9 +29,11 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -46,12 +53,44 @@ public class Registro extends AppCompatActivity {
 
     private Credential credential = null;
     private LocationManager locationManager;
-    private LocationListener locationListener;
+    private LocationListener locationListener = new MyLocationLister();
 
-    FusedLocationProviderClient fusedLocationProviderClient;
+    private MaterialButton getLocation;
 
+    String latitude;
+    String longitude;
+
+    private boolean gpsEnable = false;
+    private boolean networkEnable = false;
+    Geocoder geocoder;
+    List<Address> addresses;
+
+    /*@Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(Registro.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }*/
 
     public void onSaveButton(View view) {
+        String cedula = empleadoCedula.getText().toString();
+        String nombres = nombresEmpleado.getText().toString();
+        String apellidos = empleadoApellido.getText().toString();
+        String correo = empleadoCorreo.getText().toString();
+        String celular = empleadoCelular.getText().toString();
+        String usuario = empleadoUsuario.getText().toString();
+        String clave = empleadoClave.getText().toString();
+        String clave2 = empleadoClave2.getText().toString();
+
+        if (credential.verificarCedula(cedula)) {
+            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Cedula incorrecta, ingrese una cedula correcta", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -61,67 +100,6 @@ public class Registro extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void getLocation(View view) {
-        if (ActivityCompat.checkSelfPermission(Registro.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            //onGetLocation();
-            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    Location location = task.getResult();
-                    if (location == null) {
-
-                        try {
-                            Geocoder geocoder = new Geocoder(Registro.this, Locale.getDefault());
-                            List<Address> addresses = geocoder.getFromLocation(
-                                    location.getLatitude(), location.getLongitude(), 1
-                            );
-
-                            empleadoLatitud.setText((int) addresses.get(0).getLatitude());
-                            empleadoLongitud.setText((int) addresses.get(0).getLongitude());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-        } else {
-            ActivityCompat.requestPermissions(Registro.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
-    }
-
-    /*private void onGetLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if (location == null) {
-
-                    try {
-                        Geocoder geocoder = new Geocoder(Registro.this, Locale.getDefault());
-                        List<Address> addresses = geocoder.getFromLocation(
-                                location.getLatitude(), location.getLongitude(), 1
-                        );
-
-                        empleadoLatitud.setText((int) addresses.get(0).getLatitude());
-                        empleadoLongitud.setText((int) addresses.get(0).getLongitude());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,9 +117,152 @@ public class Registro extends AppCompatActivity {
         empleadoLatitud = (TextInputEditText) findViewById(R.id.empleadoLatitud);
         empleadoLongitud = (TextInputEditText) findViewById(R.id.empleadoLongitud);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(Registro.this);
+        getLocation = (MaterialButton) findViewById(R.id.getLocation);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMyLocation();
+            }
+        });
+
+        checkLocationPermission();
+        /*locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i("location: ", location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(Registro.this, Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(Registro.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }*/
 
 
+    }
+
+    private boolean checkLocationPermission() {
+
+        int location = ContextCompat.checkSelfPermission(Registro.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int location2 = ContextCompat.checkSelfPermission(Registro.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        List<String> listPermission = new ArrayList<>();
+
+        if (location != PackageManager.PERMISSION_GRANTED){
+            listPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (location2 != PackageManager.PERMISSION_GRANTED){
+            listPermission.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (listPermission.isEmpty()){
+            ActivityCompat.requestPermissions(Registro.this,listPermission.toArray(new String[listPermission.size()]),
+                    1);
+        }
+
+        return true;
+    }
+
+    public void getMyLocation() {
+
+        try {
+            gpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            networkEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!gpsEnable && !networkEnable) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+            builder.setTitle("Alerta");
+            builder.setMessage("La ubicación no está disponible");
+            builder.create().show();
+        }
+
+        if (gpsEnable) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+
+        if (networkEnable){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+
+
+
+
+    }
+
+    class MyLocationLister implements LocationListener {
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location == null){
+                locationManager.removeUpdates(locationListener);
+                latitude = ""+location.getLatitude();
+                longitude = ""+location.getLongitude();
+
+                empleadoLatitud.setText(latitude);
+                empleadoLongitud.setText(longitude);
+
+
+                geocoder = new Geocoder(Registro.this, Locale.getDefault());
+                try {
+                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String address = addresses.get(0).getAddressLine(0);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
     }
 
 
